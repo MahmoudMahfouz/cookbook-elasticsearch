6f16d4d59307dec39b610f57d77dc51731c14221
Mahmoud Mahfouz <mahmoud.mahfouz.zaza@gmail.com>
2015-12-30 13:16:08 +0200

    ES configuration for Vagrant

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # A fork of [Elasticsearch cookbook](https://github.com/elastic/cookbook-elasticsearch)
 
-Required attributes:
-node['elasticsearch']['cluster']['name']
-node['elasticsearch']['node']['name']
-node['elasticsearch']['node']['index_buffer_size']
-node['elasticsearch']['aws']['access_key']
-node['elasticsearch']['aws']['secret_key']
-node['elasticsearch']['aws']['availability_zone']
-node['elasticsearch']['aws']['region']
+Use node['elasticsearch']['config'] to build the configurations of the elasticsearch.yml file
--- recipes/ibg.rb
+++ recipes/ibg.rb
@@ -1,8 +1,5 @@
 # Encoding: utf-8
 #
-# Cookbook Name:: elasticsearch
-# Recipe:: default
-#
 
 include_recipe 'chef-sugar'
 
@@ -12,31 +9,15 @@ elasticsearch_install 'elasticsearch' do
   type node['elasticsearch']['install_type'].to_sym # since TK can't symbol.
 end
 elasticsearch_configure 'elasticsearch' do
-  configuration ({
-    'cluster.name' => node['elasticsearch']['cluster']['name'],
-    'node.name' => node['elasticsearch']['node']['name'],
-    'indicies.memory.index_buffer_size' => node['elasticsearch']['node']['index_buffer_size'],
-    'bootstrap.mlockall' => true,
-
-		'cloud.aws.access_key' => node['elasticsearch']['aws']['access_key'],
-		'cloud.aws.secret_key' => node['elasticsearch']['aws']['secret_key'],
-		'discovery.type' => "ec2",
-		'discovery.ec2.groups' => "elasticsearch cluster",
-		'discovery.ec2.host_type' => "public_ip",
-		'discovery.ec2.ping_timeout' => "30s",
-		'discovery.ec2.availability_zones' => node['elasticsearch']['aws']['availability_zone'],
-		'cloud.aws.region' => node['elasticsearch']['aws']['region'],
-		'discovery.zen.ping.multicast.enabled' => false,
-    'http.host' => 0,
-	})
+  configuration(node['elasticsearch']['config'] || {})
 end
 
 elasticsearch_service 'elasticsearch'
 
 # by default, no plugins
 elasticsearch_plugin 'kopf' do
-	url 'lmenezes/elasticsearch-kopf'
-	action :install
+  url 'lmenezes/elasticsearch-kopf'
+  action :install
 end
 
 elasticsearch_plugin 'head' do
@@ -54,6 +35,6 @@ elasticsearch_plugin 'HQ' do
 end
 
 elasticsearch_plugin 'AWS' do
-  url 'cloud-aws'
-  action :install
+    url 'cloud-aws'
+      action :install
 end
